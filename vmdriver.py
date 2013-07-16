3ad6d3645da2a03bb8c74c2c0eb0c08ffa7ce455
tarokkk <tarokkk@cloud.ik.bme.hu>
2013-07-16 14:49:35 +0200

    vmdriver basic functions

---
+++ vmdriver.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import libvirt
+import vm
+import logging
+
+
+class VMDriver:
+    '''Circle Virtal Machin driver main class
+    '''
+    connection = None
+
+    def req_connection(original_function):
+        '''Connection checking decorator for libvirt.
+        '''
+        def new_function(*args, **kwargs):
+            if args[0].connection is None:
+                logging.error("No connection to libvirt daemon.")
+            else:
+                return original_function(*args, **kwargs)
+        return new_function
+
+    def connect(self, connection_string='qemu:///system'):
+        if self.connection is None:
+            self.connection = libvirt.open(connection_string)
+        else:
+            logging.error("There is already an active connection to libvirt.")
+
+    @req_connection
+    def disconnect(self, connection_string='qemu:///system'):
+        self.connection.close()
+        self.connection = None
+
+    @req_connection
+    def vm_define(self, vm):
+        self.connection.defineXML(vm.dump_xml())
+        logging.info("Virtual machine %s is defined from xml", vm.name)
+
+    def vm_create(self, vm):
+        self.connection.createXML(vm.dump_xml())
+        logging.info("Virtual machine %s is created from xml", vm.name)
+
+    @req_connection
+    def list_domains(self):
+        return self.connection.listDefinedDomains()
+
+#Create VM
+a = vm.VMNetwork(name="vm-88", mac="02:00:00:00:00:00")
+b = vm.VMDisk(name="asd", source='/asdasd/adasds/asd')
+testvm = vm.VMInstance(name="Thisthename", vcpu="1",
+                       memory_max="2048",
+                       disk_list=[a],
+                       network_list=[b])
