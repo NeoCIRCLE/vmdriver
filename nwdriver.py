9df57d8f31829cabbfba7e3e74c9ff4548ae8b18
tarokkk <tarokkk@cloud.ik.bme.hu>
2013-07-17 16:34:51 +0200

    initial network driver

---
+++ nwdriver.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+
+class NWDriver:
+
+    def __init__():
+        pass
+
+    def ovs_command_execute(self, command):
+        pass
+
+    def nw_create(self, vm):
+        pass
+
+    def nw_delete(self, vm):
+        pass
