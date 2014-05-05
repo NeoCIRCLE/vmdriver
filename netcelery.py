2dce6acaf8e3a91489321a8df4c1056314c8cc14
Guba Sándor <guba.sandor@cloud.bme.hu>
2014-05-05 12:43:04 +0200

    netcelery: added argument handling to start celery dynamic instances

--- netcelery.py
+++ netcelery.py
@@ -1,20 +1,44 @@
+""" Celery module for libvirt RPC calls. """
 from celery import Celery
 from kombu import Queue, Exchange
-from socket import gethostname
 from os import getenv
-HOSTNAME = gethostname()
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-n", "--hostname", dest="hostname",
+                    help="Define the full queue name with"
+                    "with priority", metavar="hostname.queue.priority")
+(args, unknwon_args) = parser.parse_known_args()
+HOSTNAME = vars(args).pop("hostname")
+if HOSTNAME is None:
+    raise Exception("You must define hostname as -n <hostname> or "
+                    "--hostname=<hostname>.\n"
+                    "Hostname format must be hostname.module.priority.")
+
 AMQP_URI = getenv('AMQP_URI')
 CACHE_URI = getenv('CACHE_URI')
 
-celery = Celery('netdriver', backend='cache',
+
+def to_bool(value):
+    return value.lower() in ("true", "yes", "y", "t")
+
+lib_connection = None
+
+celery = Celery('netcelery',
                 broker=AMQP_URI,
                 include=['netdriver'])
 
 celery.conf.update(
+    CELERY_RESULT_BACKEND='cache',
     CELERY_CACHE_BACKEND=CACHE_URI,
     CELERY_TASK_RESULT_EXPIRES=300,
     CELERY_QUEUES=(
-        Queue(HOSTNAME + '.net', Exchange(
-            'netdriver', type='direct'), routing_key='netdriver'),
+        Queue(HOSTNAME, Exchange(
+            'netdriver', type='direct'), routing_key="netdriver"),
     )
 )
+
+if to_bool(getenv('LIBVIRT_KEEPALIVE', "False")):
+    import libvirt
+    lib_connection = libvirt.open(getenv('LIBVIRT_URI'))
